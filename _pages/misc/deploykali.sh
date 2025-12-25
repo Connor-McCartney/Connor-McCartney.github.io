b6ab06075b170c9012004e3f7f8c588d155b2944
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-12-25 22:28:40 +1100

    Update deploykali.sh

--- _pages/misc/deploykali.sh
+++ _pages/misc/deploykali.sh
@@ -2,7 +2,7 @@ set -e
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install neovim xorg libxft-dev libxinerama-dev libxtst-dev feh picom flameshot alsa-utils libasound2-dev thunar kitty konsole fzf fd-find -y
+sudo apt install neovim xorg libxft-dev libxinerama-dev libxtst-dev feh picom flameshot alsa-utils libasound2-dev thunar kitty konsole fzf fd-find tree-sitter-cli -y
 # have to change fd to fdfind in bashrc
 # alacritty is buggy in VM don't use
 # sudo visudo
